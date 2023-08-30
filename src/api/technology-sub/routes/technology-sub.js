093408557282a87185b49e449b211f410779b415
Meas Manet <mrneth58@gmail.com>
2023-08-30 10:53:06 +0700

    #Technology Category

--- src/api/technology-sub/routes/technology-sub.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * technology-sub router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::technology-sub.technology-sub');
--- src/api/technology-sub/services/technology-sub.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * technology-sub service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::technology-sub.technology-sub');
