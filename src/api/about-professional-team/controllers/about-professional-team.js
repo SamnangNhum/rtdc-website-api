1ffd026d500dcaac37821e5aa215ec2563ad28a4
Meas Manet <mrneth58@gmail.com>
2023-08-30 15:34:29 +0700

    Updates db / relation

---
+++ src/api/about-professional-team/controllers/about-professional-team.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * about-professional-team controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::about-professional-team.about-professional-team');
---
+++ src/api/about-professional-team/services/about-professional-team.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * about-professional-team service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::about-professional-team.about-professional-team');
