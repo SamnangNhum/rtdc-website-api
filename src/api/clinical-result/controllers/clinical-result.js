a79a889f12d2e4de62ec2da95d793c5b8ef5c6e2
Meas Manet <mrneth58@gmail.com>
2023-09-14 11:13:07 +0700

    #Fix

---
+++ src/api/clinical-result/controllers/clinical-result.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * clinical-result controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::clinical-result.clinical-result');
---
+++ src/api/clinical-result/services/clinical-result.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * clinical-result service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::clinical-result.clinical-result');
