419e6b6c392d62e27765b1a389809551c6c791a3
Meas Manet <mrneth58@gmail.com>
2023-10-06 09:02:52 +0700

    #Fix

--- src/api/dentist/controllers/dentist.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * dentist controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::dentist.dentist');
