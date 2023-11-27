07096a90c7b202ed89942227a06b7b468ae03cec
Meas Manet <mrneth58@gmail.com>
2023-11-27 09:28:38 +0700

    #Add Contact Submit Form

---
+++ src/api/contact-submit/controllers/contact-submit.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * contact-submit controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::contact-submit.contact-submit');
---
+++ src/api/contact-submit/services/contact-submit.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * contact-submit service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::contact-submit.contact-submit');
